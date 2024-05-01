312b4687caabeb3a0d34d3fb99989428595d8813
gabdanho <bogdanbabenko201804@gmail.com>
2024-05-02 00:17:45 +0300

    Design AddSpendScreen

--- app/src/main/java/com/example/monefy/model/Expense.kt
+++ app/src/main/java/com/example/monefy/model/Expense.kt
@@ -3,6 +3,7 @@ package com.example.monefy.model
 data class Expense(
     val categoryName: String,
     val name: String,
+    val description: String = "",
     val price: Double,
     val count: Int,
     val sumPrice: Double = price * count.toDouble()
--- app/src/main/java/com/example/monefy/screen/AddSpendScreen.kt
+++ app/src/main/java/com/example/monefy/screen/AddSpendScreen.kt
@@ -1,2 +1,183 @@
 package com.example.monefy.screen
 
+import android.content.res.Configuration
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.UiMode
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AddSpendScreen(
+    modifier: Modifier = Modifier
+) {
+    var spendName by rememberSaveable { mutableStateOf("") }
+    var count by rememberSaveable { mutableStateOf("") }
+    var spendDescription by rememberSaveable { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.padding(8.dp)
+    ) {
+        Text(text = "Название траты")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 8.dp)
+        ) {
+            TextField(
+                value = spendName,
+                onValueChange = { spendName = it },
+                maxLines = 1,
+                colors = TextFieldDefaults.textFieldColors(containerColor = Color.Transparent),
+                modifier = Modifier.weight(4f)
+            )
+            IconButton(
+                onClick = { /* TODO */ },
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "Удалить название",
+                )
+            }
+        }
+        Text(text = "Количество")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 8.dp)
+        ) {
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Уменьшить количество"
+                )
+            }
+            BasicTextField(
+                value = count,
+                onValueChange = { count = it },
+                modifier = Modifier
+                    .width(70.dp)
+            ) {
+                Text(
+                    text = "0",
+                    style = MaterialTheme.typography.headlineMedium,
+                    textAlign = TextAlign.Center
+                )
+            }
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowForward,
+                    contentDescription = "Увеличить количество"
+                )
+            }
+        }
+        Text(text = "Категория")
+        LazyHorizontalGrid(
+            rows = GridCells.Fixed(2),
+            modifier = Modifier
+                .height(maxOf(200.dp))
+                .padding(bottom = 8.dp)
+        ) {
+            items(13) { index ->
+                CategoryCard(index)
+            }
+        }
+        Text(text = "Описание")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 8.dp)
+        ) {
+            TextField(
+                value = spendDescription,
+                onValueChange = { spendDescription = it },
+                maxLines = 1,
+                colors = TextFieldDefaults.textFieldColors(containerColor = Color.Transparent),
+                modifier = Modifier.weight(4f)
+            )
+            IconButton(
+                onClick = { /* TODO */ },
+                modifier = Modifier.weight(1f)
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Close,
+                    contentDescription = "Удалить название",
+                )
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.align(Alignment.CenterHorizontally )
+        ) {
+            Text("Добавить")
+        }
+    }
+}
+
+@Composable
+fun CategoryCard(
+    index: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+        modifier = modifier
+            .size(150.dp)
+            .padding(4.dp)
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        ) {
+            Text(text = "Category category category category")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AddSpendScreenPreview() {
+    AddSpendScreen(Modifier.fillMaxSize())
+}

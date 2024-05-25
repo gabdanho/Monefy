de227e54dae0f06ba4f93db3c576a095147ef050
gabdanho <bogdanbabenko201804@gmail.com>
2024-05-26 00:00:20 +0300

    remove Spend table

--- app/src/main/java/com/example/monefy/data/Category.kt
+++ app/src/main/java/com/example/monefy/data/Category.kt
@@ -4,15 +4,15 @@ import androidx.annotation.NonNull
 import androidx.compose.ui.graphics.Color
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.monefy.model.Expense
 
 @Entity(tableName = "categories")
 data class Category(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-    @NonNull
     val name: String,
     val color: Color,
-    val spends: List<Spend>,
+    val spends: List<Expense>,
     val totalCategoryPrice: Double,
     val isTapped: Boolean = false
 )
--- app/src/main/java/com/example/monefy/data/SpendsRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.monefy.data
-
-interface SpendsRepository {
-    suspend fun insertSpend(spend: Spend)
-
-    suspend fun deleteSpend(id: Int)
-
-    suspend fun updateSpend(spend: Spend)
-}
